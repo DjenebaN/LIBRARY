239ede2589f6fbef5e535fc4d6301459f6d4dd70
Djénéba Niangadou <djeneba.ngd.school@gmail.com>
2025-03-03 21:46:12 +0100

    m

--- model/pretModel.php
+++ model/pretModel.php
@@ -2,12 +2,11 @@
 
 class Pret
 {
-
     private $bdd;
 
-    function __construct ($bdd)
+    function __construct($bdd)
     {
-        $this -> bdd = $bdd;
+        $this->bdd = $bdd;
     }
 
     public function ajouterLivreEtPret($Id_API, $Titre, $Auteur, $Annee, $Image_URL, $Id_Lecteur, $date_Emprunt, $date_retour)
@@ -27,7 +26,7 @@ class Pret
             $req->bindParam(':Annee', $Annee);
             $req->bindParam(':Image_URL', $Image_URL);
             $req->execute();
-            $Id_Livre = $this->bdd->lastInsertId();
+            $Id_Livre = $this->bdd->lastInsertId();  // Récupérer le dernier Id_Livre inséré
         }
 
         $req = $this->bdd->prepare("INSERT INTO PRET (Id_Livre, Id_Lecteur, Date_Emprunt, Date_Retour) VALUES (:Id_Livre, :Id_Lecteur, :date_Emprunt, :date_retour)");
@@ -35,9 +34,28 @@ class Pret
         $req->bindParam(':Id_Lecteur', $Id_Lecteur);
         $req->bindParam(':date_Emprunt', $date_Emprunt);
         $req->bindParam(':date_retour', $date_retour);
-        
+
+        $stmt = $this->bdd->prepare("UPDATE LIVRE SET Etat = 4 WHERE Id_Livre = :Id_Livre");
+        $stmt->bindParam(':Id_Livre', $Id_Livre, PDO::PARAM_INT);
+        $stmt->execute();
+
         return $req->execute();
     }
 
+    public function afficherPrets($Id_Lecteur)
+    {
+        $req = $this->bdd->prepare("SELECT P.Id_Pret, L.Titre, P.Date_Emprunt, P.Date_Retour, E.Etat AS Etat_Livre
+                                    FROM PRET P
+                                    JOIN LIVRE L ON P.Id_Livre = L.Id_Livre
+                                    JOIN ETAT E ON L.Etat = E.Id_Etat
+                                    WHERE P.Id_Lecteur = :Id_Lecteur");
+        $req->bindParam(':Id_Lecteur', $Id_Lecteur, PDO::PARAM_INT);
+        $req->execute();
+        $emprunts = $req->fetchAll(PDO::FETCH_ASSOC);
+
+        return $emprunts;
+    }
 
 }
+
+?>
--- view/compte.php
+++ view/compte.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once $_SERVER['DOCUMENT_ROOT'] . '/LIBRARY/model/lecteurModel.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/LIBRARY/model/pretModel.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/LIBRARY/bdd/bdd.php';
 
 if (isset($_SESSION['Id_Lecteur'])) {
@@ -12,11 +13,17 @@ if (isset($_SESSION['Id_Lecteur'])) {
 }
 
 $lecteur = new Lecteur ($bdd);
-
 $detailLecteur = $lecteur -> unLecteur($ID);
+
+$prets = new Pret($bdd);
+$mesPrets = $prets -> afficherPrets($ID);
 
 echo '<p>Nom : ' . htmlspecialchars($detailLecteur['Nom']) . '</p>';
 echo '<p>Prenom : ' . htmlspecialchars($detailLecteur['Prenom']) . '</p>';
 echo '<p>Email : ' . htmlspecialchars($detailLecteur['Email']) . '</p>';
+
+foreach ($mesPrets as $pret) {
+    echo "Livre : " . htmlspecialchars($pret['Titre']) . "</br>";
+}
 
 ?>
